0b9bc523ace21d36bd2000c4589c0cd819f2ae18
CNife <CNife@vip.qq.com>
2022-07-11 18:39:55 +0800

    使用Python重写new-solution

---
+++ bin/new-solution.py
@@ -0,0 +1,55 @@
+import subprocess
+from enum import Enum
+from pathlib import Path
+
+import typer
+
+COUNTS = {
+    "_i": "_1",
+    "_ii": "_2",
+    "_iii": "_3",
+    "_iv": "_4",
+    "_v": "_5",
+}
+
+
+class IDE(Enum):
+    GOLAND = "goland"
+    VSCODE = "vscode"
+
+
+IDE_COMMANDS = {
+    IDE.GOLAND: "goland",
+    IDE.VSCODE: "code",
+}
+
+
+def main(directory: Path, title: str, ide: IDE = typer.Option(...)):
+    title = title.strip()
+    title = title.replace("-", "_")
+    for roma, india in COUNTS.items():
+        if title.endswith(roma):
+            title.replace(roma, india)
+
+    subdir = directory / "code" / title
+    subdir.mkdir(exist_ok=True)
+    firstLine = f"package {title}\n"
+    codeFile = subdir / "solution.go"
+    codeFile.write_text(firstLine)
+    testFile = subdir / "solution_test.go"
+    testFile.write_text(firstLine)
+
+    subprocess.run(
+        [IDE_COMMANDS[ide], str(directory), str(codeFile)],
+        shell=True,
+        check=True,
+    )
+    subprocess.run(
+        [IDE_COMMANDS[ide], str(directory), str(testFile)],
+        shell=True,
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
--- new-solution.ps1
+++ new-solution.ps1
@@ -1,20 +1,4 @@
-Param([String] $Title)
+﻿Param([String] $Title, [String] $IDE)
 
-$Title = $Title.Trim()
-$Title = $Title -replace '-', '_'
-
-if ($Title -match '_(i{1,3})$')
-{
-    $Title = $Title -replace '_(i{1,3})$', "_$( $Matches[1].Length )"
-}
-$Title = $Title -replace '_iv$', '_4'
-$Title = $Title -replace '_v$', '_5'
-
-$SolutionDir = "$PSScriptRoot\code\$Title"
-New-Item $SolutionDir -ItemType Directory | Out-Null
-
-$SolutionSrc = "$SolutionDir\solution.go"
-$SolutionTest = "$SolutionDir\solution_test.go"
-Write-Output "package $Title`n" | Tee-Object $SolutionSrc | Out-File $SolutionTest
-goland $PSScriptRoot $SolutionSrc
-goland $PSScriptRoot $SolutionTest
+. $PSScriptRoot/bin/.venv/Scripts/Activate.ps1
+python $PSScriptRoot/bin/new-solution.py $PSScriptRoot $Title --ide $IDE
